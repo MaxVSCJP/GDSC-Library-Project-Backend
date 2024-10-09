ab0c2402837165ff82272b4c11ddfc419903c59d
Hansonket <139464736+MaxVSCJP@users.noreply.github.com>
2024-10-09 22:15:29 +0300

    Setup server to be deployed using netlify

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
 const xssClean = require("xss-clean");
 require("dotenv").config();
 const app = express();
@@ -14,52 +15,51 @@ const UserRoutes = require("./Routing/UserRoutes");
 const SearchRoute = require("./Routing/SearchRoute");
 
 const corsOptions = {
-    origin: "*",
-    optionsSuccessStatus: 200
-}
-
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 
 const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 200    
+  windowMs: 10 * 60 * 1000,
+  max: 200,
 });
 
-
-app.use(helmet({
+app.use(
+  helmet({
     contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'"],
-            objectSrc: ["'none'"]
-        }
-    }
-}));
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        objectSrc: ["'none'"],
+      },
+    },
+  })
+);
 
 app.use(xssClean());
 app.use(limiter);
 app.use(express.json());
 app.use(mongoSanitize());
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use("/books", BookRoutes);
 app.use("/auth", AuthRoutes);
 app.use("/user", UserRoutes);
 app.use("/search", SearchRoute);
 app.get("/*", (req, res) => {
-    res.status(200).json({message: "Welcome to the API"});
+  res.status(200).json({ message: "Welcome to the API" });
 });
 
-
-
 (async () => {
-    try{
-        await mongoose.connect(process.env.MONGODB_URI)
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
 
-        console.log("Connected to Database");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    }
-    catch(err){
-        console.log(err);
-    }
+    console.log("Connected to Database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 })();
+
+export default handler = serverless(app);
