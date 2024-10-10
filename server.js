4f4141a85210e6b440c86ecdb16daa4e70aa8738
Hansonket <139464736+MaxVSCJP@users.noreply.github.com>
2024-10-10 10:22:51 +0300

    Trying to change up deployment configurations

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const xssClean = require("xss-clean");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 1738;
-const BookRoutes = require("../../Routing/BookRoutes");
-const AuthRoutes = require("../../Routing/AuthRoutes");
-const UserRoutes = require("../../Routing/UserRoutes");
-const SearchRoute = require("../../Routing/SearchRoute");
+const BookRoutes = require("./Routing/BookRoutes");
+const AuthRoutes = require("./Routing/AuthRoutes");
+const UserRoutes = require("./Routing/UserRoutes");
+const SearchRoute = require("./Routing/SearchRoute");
 
 let isConnected = false; // Track if MongoDB is already connected
 
