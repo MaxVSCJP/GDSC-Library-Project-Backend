ec43550e8b5c370e8995f5bf5cd4aef56a935583
Maassas Michael <139464736+MaxVSCJP@users.noreply.github.com>
2024-11-23 03:18:54 +0300

    Added some debugging to csrfProtection
    Added some debugging console logs to the csrf Protection middleware to see why it's working during development and not production

--- Middlewares/CSRFProtectionMW.js
+++ Middlewares/CSRFProtectionMW.js
@@ -16,9 +16,12 @@ const { doubleCsrfProtection, generateToken } = doubleCsrf({
 
 function csrfMiddleware(req, res, next) {
   doubleCsrfProtection(req, res, (err) => {
+    console.log("No errors: ", req.cookies);
+    console.log("No errors: ", req.headers["x-csrf-token"]);
     if (err) {
       console.log(req.cookies);
       console.error("CSRF validation error:", err);
+      console.log(req.headers["x-csrf-token"]);
       if (err.code === "EBADCSRFTOKEN") {
         return res.status(403).json({ error: "Invalid or missing CSRF token" });
       }
