684ae8be3edb538b5dd153c79f63fa98e846e81c
Maassas Michael <139464736+MaxVSCJP@users.noreply.github.com>
2024-11-23 04:04:35 +0300

    Added better debugging to CSRF Protection Middleware

--- Middlewares/CSRFProtectionMW.js
+++ Middlewares/CSRFProtectionMW.js
@@ -16,12 +16,15 @@ const { doubleCsrfProtection, generateToken } = doubleCsrf({
 
 function csrfMiddleware(req, res, next) {
   doubleCsrfProtection(req, res, (err) => {
-    console.log("No errors: ", req.cookies);
-    console.log("No errors: ", req.headers["x-csrf-token"]);
+    console.error("CSRF Token from header: ", req.headers["x-csrf-token"]);
+    console.error("CSRF Token from cookie: ", req.cookies.csrfToken);
     if (err) {
       console.log(req.cookies);
-      console.error("CSRF validation error:", err);
-      console.log(req.headers["x-csrf-token"]);
+      console.error(
+        "Error, CSRF Token from header: ",
+        req.headers["x-csrf-token"]
+      );
+      console.error("Error, CSRF Token from cookie: ", req.cookies.csrfToken);
       if (err.code === "EBADCSRFTOKEN") {
         return res.status(403).json({ error: "Invalid or missing CSRF token" });
       }
